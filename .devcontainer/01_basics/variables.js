e278457751aa1d0f90a42eb22e92e9fa93758133
Ayush Shrivastava <2k22.cse.2213290@gmail.com>
2025-08-22 08:23:54 +0000

    variables

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 13423
+let accountEmail = "ayushshrivastav865@gmail.com"
+var password="123456"
+accountcity="jhansi"
+
+accountEmail = " 2k22.cse.2213290@gmail.com"
+password="897654"
+accountcity="kanpur"
+// prefer not to use var , issue in scope{ 
+// } and functional block 
+console.table([accountEmail,accountId,accountcity,password])
+
