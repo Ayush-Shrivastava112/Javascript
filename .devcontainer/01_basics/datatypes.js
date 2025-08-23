1583d0a0a85c0bdf3d7b28cb8d0e66d02171f844
Ayush Shrivastava <2k22.cse.2213290@gmail.com>
2025-08-23 12:00:39 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,11 @@
+let name ="Ayush"
+let age = 21
+let isLoggedIn = false 
+let state; // undefined
+//number => 2 to power 53
+// bigint
+// string=> ""
+//boolean => True / False
+//null => standalone value (empty)
+//undefined 
+console.log(typeof age);
