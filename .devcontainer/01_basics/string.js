017eb81187d08b45e0336bfcfb0ac6fc77424122
Ayush Shrivastava <2k22.cse.2213290@gmail.com>
2025-08-25 05:51:47 +0000

    strings

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,12 @@
+const name = "Ayush"
+const repoCount = 50
+
+// OLD FASHIONED ,console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gamename = new String("Aysuhqecg")
+console.log(gamename[0]);
+console.log(gamename._proto_);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
